e27db63da753e67d193433c0e21643a54293680d
Dominovsky <hejdominowy@outlook.com>
2020-11-15 10:34:48 +0100

    Com

---
+++ Kolokwium.1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Kolokwium._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
