ca49014743ef2bb1b92d8bc492bc2d13f1e16323
Dominovsky <hejdominowy@outlook.com>
2020-11-15 11:15:56 +0100

    com

--- Kolokwium.1/Program.cs
+++ Kolokwium.1/Program.cs
@@ -1,12 +1,36 @@
 ﻿using System;
+using System.Threading;
 
 namespace Kolokwium._1
 {
+    class Post
+    {
+        public string Autor;
+        public string Tytul;
+        public string Tresc;
+        public string Tagi;
+        
+        public Post(string tytul, string autor,string tresc, string tagi)
+        {
+            Tytul = tytul;
+            Autor = autor;
+            Tresc = tresc;
+            Tagi = tagi;
+
+        }
+    }
+    
     class Program
     {
+        static void wczytaj(string imie, string nazwisko, int wiek)
+        {
+            Console.WriteLine("Imie: {0} \n Nazwisko: {1} \n Wiek: {2}"
+                , imie, nazwisko, wiek);
+        }
+        
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            wczytaj("Dominik", "Katana", 20);
         }
     }
 }
